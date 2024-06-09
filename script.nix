a64c44fd01df6b25e0baab41a5405d49518d1862
haennes <hannes.hofmuth@gmail.com>
2024-06-09 22:41:22 +0200

    writeShellScript

--- script.nix
+++ script.nix
@@ -1,5 +1,5 @@
 {pkgs, out_path, model, name, ...}:
-  pkgs.writeScriptBin name
-        ''#!/bin/sh
+  pkgs.writeShellScriptBin name
+        ''
            ffmpeg -i ~/signal-*.mp3 -y -ar 16000 /tmp/out.wav -v 24 -stats
            whisper-cpp /tmp/out.wav -otxt -l de -m  ${out_path}/${model} --print-progress''
