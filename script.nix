c36638d47f37b807e208ec143c402584bca7a627
haennes <hannes.hofmuth@gmail.com>
2024-06-17 23:15:46 +0200

    should work with all files now

--- script.nix
+++ script.nix
@@ -1,5 +1,13 @@
 {pkgs, out_path, model, name, ...}:
   pkgs.writeShellScriptBin name
         ''
-           ffmpeg -i ~/signal-*.mp3 -y -ar 16000 /tmp/out.wav -v 24 -stats
-           whisper-cpp /tmp/out.wav -otxt -l de -m  ${out_path}/${model} --print-progress''
+           IFS=$'\n\t'
+           path=$HOME
+           files=$(ls $path -1 | grep -E '^(signal|Sprachnachricht).*(m4a|mp3|aac)$')
+           echo $files
+           for file in $files; do
+             ffmpeg -i "$path/$file" -y -ar 16000 /tmp/out.wav -v 24 -stats
+             whisper-cpp /tmp/out.wav -otxt -of "$path/$file" -l de -m  ${out_path}/${model} --print-progress
+             mv  "$path/$file" "$path/processed_$file"
+           done
+             ''
